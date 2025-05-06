3965ce5ff7fc72aba2d8d3daec6a87ece7feca19
IslamMohamedSalama <mstkhdmjwjl17@gmail.com>
2025-05-06 17:40:09 +0300

    Fix A Big Problem On THis Page And Use Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -8,30 +8,30 @@ import gulpSass from "gulp-sass";
 import srcmap from "gulp-sourcemaps";
 import terser from "gulp-terser";
 import notify from "gulp-notify";
-import zip from "gulp-zip";
+// import zip from "gulp-zip";
 const sass = gulpSass(dartSass);
 
 gulp.task("html", () => {
 	return gulp
-		.src("project/pug/index.pug")
+		.src("pug/index.pug")
 		.pipe(pug())
-		.pipe(gulp.dest("dist"))
+		.pipe(gulp.dest("."))
 		.pipe(notify());
 });
 gulp.task("js", () => {
 	return gulp
-		.src("project/js/**/*.js")
+		.src("js/**/*.js")
 		.pipe(srcmap.init())
 		.pipe(terser())
 		.pipe(gulpConcat("script.min.js"))
 		.pipe(srcmap.write("./maps"))
-		.pipe(gulp.dest("dist/js"))
+		.pipe(gulp.dest("js"))
 		.pipe(notify());
 });
 
 gulp.task("css", () => {
 	return gulp
-		.src("project/css/**/*.scss")
+		.src(["css/**/*.scss","css/**/*.css"])
 		.pipe(srcmap.init())
 		.pipe(
 			autoprefixer({
@@ -42,21 +42,14 @@ gulp.task("css", () => {
 		.pipe(sass({ style: "compressed" }))
 		.pipe(gulpConcat("style.min.css"))
 		.pipe(srcmap.write("./maps"))
-		.pipe(gulp.dest("dist/css"))
+		.pipe(gulp.dest("css"))
 		.pipe(notify());
 });
 
-gulp.task("compress", () => {
-	return gulp.src("dist/**/*.*").pipe(zip("website.zip")).pipe(gulp.dest("."));
-});
-
 gulp.task("watch", () => {
-	gulp.watch("project/*.pug", gulp.series("html"));
-	gulp.watch("project/css/**/*.scss", gulp.series("css"));
-	gulp.watch("project/js/**/*.js", gulp.series("js"));
-});
-gulp.task("moveAssets", () => {
-	return gulp.src(["project/webfonts","project/assets"]).pipe(gulp.dest("dist"))
+	gulp.watch("*.pug", gulp.series("html"));
+	gulp.watch("css/**/*.scss", gulp.series("css"));
+	gulp.watch("js/**/*.js", gulp.series("js"));
 });
 
 gulp.task("default", gulp.series("watch"));

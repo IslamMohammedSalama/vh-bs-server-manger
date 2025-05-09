a1e127a2901075821c746eb2bf584d0e4c8c59d7
IslamMohamedSalama <mstkhdmjwjl17@gmail.com>
2025-05-09 09:49:41 +0300

    fix: add repository URL to publish options in gh-pages deployment

--- node.js
+++ node.js
@@ -1,5 +1,5 @@
 import { publish } from "gh-pages";
 
-publish("dist",{dest:"."}, function (err) {
+publish("dist",{dest:".",repo:"https://github.com/IslamMohammedSalama/vh-bs-server-manger.git",}, function (err) {
   console.log(err)
 });

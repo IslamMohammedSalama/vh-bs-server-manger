a7e6bde6bccdda6606a1109740f193e2d74470cf
IslamMohamedSalama <mstkhdmjwjl17@gmail.com>
2025-06-15 21:10:49 +0300

    - Add Dark Mode And Light Mode Support - Storage User Changes On Local Storage - Add Change Password Option - Make Gulp Code Better

--- js/dark_mode.js
+++
@@ -1,7 +0,0 @@
-let themeName = localStorage.getItem("theme");
-if (themeName === "dark") {
-	document.body.classList.add("dark-mode");
-	document.getElementById("dark-mode").checked = true;
-} else if (themeName === "light") {
-	document.getElementById("dark-mode").checked = false;
-}
